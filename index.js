c16ba9a4949d473c731f95cd2bdbf3a5a23637d4
gamer-angel05 <45619299+gamer-angel05@users.noreply.github.com>
2022-02-23 06:29:30 -0600

    Fix content
    Fix indentation
    Update style.css

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const publicSpreadsheetDoc = "https://opensheet.elk.sh/1-XABpNzY6jgg_Bh9KaDKS-pPNgGF22d_yAxqKOAM6RI/FoE%20Tips"; // opensource redirect for google sheet w/o auth
 
 function __init__() {
-	/*	Load the public sheet data, add anchorJS and
-		if there's an anchor hash, reload to force scroll
-		after content has been loaded.
-	*/
-	fetch(publicSpreadsheetDoc)
-	.then( response => response.json())
-	.then( data => {
-		new formatData(data);
-		anchors.options.visible = "touch";
-		anchors.add("#scroll-content h2, #scroll-content h4");
-		
-		setTimeout(function() {
-			if (window.location.hash) {
-				const hash = window.location.hash;
-				window.location.hash = "";
-				window.location.hash = hash;
-			}
-		}, 200);
-	})
+    /*  Load the public sheet data, add anchorJS and
+        if there's an anchor hash, reload to force scroll
+        after content has been loaded.
+    */
+    fetch(publicSpreadsheetDoc)
+    .then( response => response.json())
+    .then( data => {
+        new formatData(data);
+        anchors.options.visible = "touch";
+        anchors.add("#scroll-content h2, #scroll-content h4");
+        
+        setTimeout(function() {
+            if (window.location.hash) {
+                const hash = window.location.hash;
+                window.location.hash = "";
+                window.location.hash = hash;
+            }
+        }, 200);
+    })
 }
 
 $(document).scroll(function(){
-	/*	Sticky navigation change bg color on scroll,
-		display back to top button.
-	*/
+    /*  Sticky navigation change bg color on scroll,
+        display back to top button.
+    */
     var scroll = $(window).scrollTop();
     if(scroll < 300){
-    	$(".sticky-top")[0].classList.remove("active-sticky");
-    	$("#js-top").css("display", "none");
+        $(".sticky-top")[0].classList.remove("active-sticky");
+        $("#js-top").css("display", "none");
     } else{
-    	$(".sticky-top")[0].classList.add("active-sticky");
-    	$("#js-top").css("display", "");
+        $(".sticky-top")[0].classList.add("active-sticky");
+        $("#js-top").css("display", "");
     }
 });
 
--- js/main.js
+++ js/main.js
@@ -1,83 +1,83 @@
 class formatData {
-	constructor(documentation) {
-		this.addData(documentation);
-	}
+    constructor(documentation) {
+        this.addData(documentation);
+    }
 
-	addData(documentation) {
-		/*	Add the data to drop menu and scroll
-		*/
-		let topics = [...new Set(documentation.map(({Topic}) => Topic))];
+    addData(documentation) {
+        /*  Add the data to drop menu and scroll
+        */
+        let topics = [...new Set(documentation.map(({Topic}) => Topic))];
 
-		topics.forEach((topic, topic_index) => {
-			if (!topic) return
+        topics.forEach((topic, topic_index) => {
+            if (!topic) return
 
-			let article_href = topic.replace(/ /g, "-").toLowerCase()
-			this.addDropDownMenu(topic, article_href);
-			$("#scroll-content").append('<h2 id="' + article_href + '">' + topic + '</h2>');
+            let article_href = topic.replace(/ /g, "-").toLowerCase()
+            this.addDropDownMenu(topic, article_href);
+            $("#scroll-content").append('<h2 id="' + article_href + '">' + topic + '</h2>');
 
-			let articles = documentation.filter((e) => e.Topic === topic);
-			articles.forEach((article, index) => {
-				let tags = article.Tags ? article.Tags.replace(/\n/g, "").split(",") : [];
+            let articles = documentation.filter((e) => e.Topic === topic);
+            articles.forEach((article, index) => {
+                let tags = article.Tags ? article.Tags.replace(/\n/g, "").split(",") : [];
 
-				if (article.Text) {
-					let href = article_href;
+                if (article.Text) {
+                    let href = article_href;
 
-					if (article.Article) {
-						href += "-" + article.Article.replace(/ /g, "-").toLowerCase();
-						this.addDropDownMenu("--" + article.Article, href);
-					};
-					article.Text = this.substituteTags(article.Text, tags);
-					this.addContent(article.Article, article.Text, href);
-				}
-			});
-			$("#scroll-content").append('<hr class="divider">');
-		});
-	}
+                    if (article.Article) {
+                        href += "-" + article.Article.replace(/ /g, "-").toLowerCase();
+                        this.addDropDownMenu("--" + article.Article, href);
+                    };
+                    article.Text = this.substituteTags(article.Text, tags);
+                    this.addContent(article.Article, article.Text, href);
+                }
+            });
+            $("#scroll-content").append('<hr class="divider">');
+        });
+    }
 
-	substituteTags(text, tags) {
-	    let matches = text.match(/\[(.*?)\]/);
+    substituteTags(text, tags) {
+        let matches = text.match(/\[(.*?)\]/);
 
-	    if (matches && tags) {
-	        let result = "";
-	        let label = matches[1].split(":", 2);
+        if (matches && tags) {
+            let result = "";
+            let label = matches[1].split(":", 2);
 
-	        switch (label[0]) {
-	            case 'image':
-	                result = this.getImage(tags.shift());
-	                break;
-	            case 'link':
-	                result = this.getLink(tags.shift(), label[1]);
-	                break;
-	        }
-	        text = text.replace(/\[(.*?)\]/, result);
-	        return this.substituteTags(text, tags);
-	    }
-	    return text
-	}
+            switch (label[0]) {
+                case 'image':
+                    result = this.getImage(tags.shift());
+                    break;
+                case 'link':
+                    result = this.getLink(tags.shift(), label[1]);
+                    break;
+            }
+            text = text.replace(/\[(.*?)\]/, result);
+            return this.substituteTags(text, tags);
+        }
+        return text
+    }
 
-	getImage(url) {
-		if (!url) return ""
+    getImage(url) {
+        if (!url) return ""
 
-		return '<a href="' + url + '" data-toggle="lightbox"><img src="' + url + '" class="img-max" /></a>';
-	}
+        return '<a href="' + url + '" data-toggle="lightbox"><img src="' + url + '" class="img-max" /></a>';
+    }
 
-	getLink(url, title) {
-		if (!url) return title
+    getLink(url, title) {
+        if (!url) return title
 
-		if (url.startsWith('#')) {
-			return '<a href="' + url + '">' + (title || url || "link") + '</a>';
-		}
-		return '<a target="_blank" href="' + url + '">' + (title || url || "link") + '</a>';
-	}
+        if (url.startsWith('#')) {
+            return '<a href="' + url + '">' + (title || url || "link") + '</a>';
+        }
+        return '<a target="_blank" href="' + url + '">' + (title || url || "link") + '</a>';
+    }
 
-	addDropDownMenu(title, index) {
-		let link = '<a class="dropdown-item" href="#' + index + '">' + title + '</a>';
-		$("#scroll-dropmenu").append(link);
-	}
+    addDropDownMenu(title, index) {
+        let link = '<a class="dropdown-item" href="#' + index + '">' + title + '</a>';
+        $("#scroll-dropmenu").append(link);
+    }
 
-	addContent(title, content, index) {
-		var s = title ? '<h4 id="' + index + '">' + title + '</h4>' : "";
-		s += '<p style="white-space: pre-wrap;">' + content + '</p>';
-		$("#scroll-content").append(s);
-	}
+    addContent(title, content, index) {
+        var s = title ? '<h4 id="' + index + '">' + title + '</h4>' : "";
+        s += '<p style="white-space: pre-wrap;">' + content + '</p>';
+        $("#scroll-content").append(s);
+    }
 }
