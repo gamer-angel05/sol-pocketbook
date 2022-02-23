ddc9d0c9381b3b8e08a2512447194e32ac96e856
gamer-angel05 <45619299+gamer-angel05@users.noreply.github.com>
2022-02-22 21:27:08 -0600

    Fix bad bootstrap css
    Clean code

---
+++ index.js
@@ -0,0 +1,41 @@
+const publicSpreadsheetDoc = "https://opensheet.elk.sh/1-XABpNzY6jgg_Bh9KaDKS-pPNgGF22d_yAxqKOAM6RI/FoE%20Tips"; // opensource redirect for google sheet w/o auth
+
+function __init__() {
+	/*	Load the public sheet data, add anchorJS and
+		if there's an anchor hash, reload to force scroll
+		after content has been loaded.
+	*/
+	var url = window.location.hash;
+	fetch(publicSpreadsheetDoc)
+	.then( response => response.json())
+	.then( data => {
+		new formatData(data);
+		anchors.options.visible = "touch";
+		anchors.add("#scroll-content h2, #scroll-content h4");
+		if (url) {
+			window.location.hash = url;
+		}
+	})
+}
+
+$(document).scroll(function(){
+	/*	Sticky navigation change bg color on scroll,
+		display back to top button.
+	*/
+    var scroll = $(window).scrollTop();
+    if(scroll < 300){
+    	$(".sticky-top")[0].classList.remove("active-sticky");
+    	$("#js-top").css("display", "none");
+    } else{
+    	$(".sticky-top")[0].classList.add("active-sticky");
+    	$("#js-top").css("display", "");
+    }
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
+
+
+$(document).ready(__init__);
--- js/main.js
+++ js/main.js
@@ -1,81 +1,83 @@
-let publicSpreadsheetDoc = "https://opensheet.elk.sh/1-XABpNzY6jgg_Bh9KaDKS-pPNgGF22d_yAxqKOAM6RI/FoE%20Tips"; // opensource redirect for google sheet w/o auth
-
-function __init__() {
-	/*	Load the public sheet data and cache it.
-	*/
-	var url = window.location.hash;
-	fetch(publicSpreadsheetDoc)
-	.then( response => response.json())
-	.then( data => {
-		add_data(data);
-		if (url) {
-			window.location.hash = url;
-		};
-	});
-}
-
-function add_data(documentation) {
-	/*	Add the data to drop menu and scroll
-	*/
-	let topics = [...new Set(documentation.map(({Topic}) => Topic))];
-
-	topics.forEach((topic, topic_index) => {
-		if (!topic) return
-
-		let article_href = topic.replace(/ /g, "-").toLowerCase()
-		add_dropdownmenu(topic, article_href);
-		$("#scroll_content").append("<h2 id=" + article_href + ">" + topic + "</h2>");
-
-		let articles = documentation.filter((e) => e.Topic === topic);
-		articles.forEach((article, index) => {
-			let tags = article.Tags ? article.Tags.replace(/\n/g, "").split(",") : [];
-
-			if (article.Text) {
-				let href = article_href;
-
-				if (article.Article) {
-					href += "-" + article.Article.replace(/ /g, "-").toLowerCase();
-					add_dropdownmenu("--" + article.Article, href);
-				};
-				article.Text = substitute_tags(article.Text, tags);
-				add_content(article.Article, article.Text, href);
-			}
-		});
-		$("#scroll_content").append('<hr class="divider">');
-	});
-}
-
-function get_image(url) {
-	if (!url) return ""
-
-	return '<a href="' + url + '" data-toggle="lightbox"><img src="' + url + '" class="img-max" /></a>';
-}
-
-function get_link(url, title) {
-	if (!url) return title
-
-	if (url.startsWith('#')) {
-		return '<a href="' + url + '">' + (title || url || "link") + '</a>';
+class formatData {
+	constructor(documentation) {
+		this.addData(documentation);
 	}
-	return '<a target="_blank" href="' + url + '">' + (title || url || "link") + '</a>';
+
+	addData(documentation) {
+		/*	Add the data to drop menu and scroll
+		*/
+		let topics = [...new Set(documentation.map(({Topic}) => Topic))];
+
+		topics.forEach((topic, topic_index) => {
+			if (!topic) return
+
+			let article_href = topic.replace(/ /g, "-").toLowerCase()
+			this.addDropDownMenu(topic, article_href);
+			$("#scroll-content").append('<h2 id="' + article_href + '" class="article-href">' + topic + '</h2>');
+
+			let articles = documentation.filter((e) => e.Topic === topic);
+			articles.forEach((article, index) => {
+				let tags = article.Tags ? article.Tags.replace(/\n/g, "").split(",") : [];
+
+				if (article.Text) {
+					let href = article_href;
+
+					if (article.Article) {
+						href += "-" + article.Article.replace(/ /g, "-").toLowerCase();
+						this.addDropDownMenu("--" + article.Article, href);
+					};
+					article.Text = this.substituteTags(article.Text, tags);
+					this.addContent(article.Article, article.Text, href);
+				}
+			});
+			$("#scroll-content").append('<hr class="divider">');
+		});
+	}
+
+	substituteTags(text, tags) {
+	    let matches = text.match(/\[(.*?)\]/);
+
+	    if (matches && tags) {
+	        let result = "";
+	        let label = matches[1].split(":", 2);
+
+	        switch (label[0]) {
+	            case 'image':
+	                result = this.getImage(tags.shift());
+	                break;
+	            case 'link':
+	                result = this.getLink(tags.shift(), label[1]);
+	                break;
+	        }
+	        text = text.replace(/\[(.*?)\]/, result);
+	        return this.substituteTags(text, tags);
+	    }
+	    return text
+	}
+
+	getImage(url) {
+		if (!url) return ""
+
+		return '<a href="' + url + '" data-toggle="lightbox"><img src="' + url + '" class="img-max" /></a>';
+	}
+
+	getLink(url, title) {
+		if (!url) return title
+
+		if (url.startsWith('#')) {
+			return '<a href="' + url + '">' + (title || url || "link") + '</a>';
+		}
+		return '<a target="_blank" href="' + url + '">' + (title || url || "link") + '</a>';
+	}
+
+	addDropDownMenu(title, index) {
+		let link = '<a class="dropdown-item" href="#' + index + '">' + title + '</a>';
+		$("#scroll-dropmenu").append(link);
+	}
+
+	addContent(title, content, index) {
+		var s = title ? '<h4 id="' + index + '" class="article-href">' + title + '</h4>' : "";
+		s += '<p style="white-space: pre-wrap;">' + content + '</p>';
+		$("#scroll-content").append(s);
+	}
 }
-
-function add_dropdownmenu(title, index) {
-	let link = '<a class="dropdown-item" href="#' + index + '">' + title + '</a>';
-	$("#scroll_dropmenu").append(link);
-}
-
-function add_content(title, content, index) {
-	var s = '<h4 id="' + index + '">' + title + '</h4>';
-	s += '<p style="white-space: pre-wrap;">' + content + '</p>';
-	$("#scroll_content").append(s);
-}
-
-
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-                event.preventDefault();
-                $(this).ekkoLightbox();
-            });
-
-
-$(document).ready(__init__);
