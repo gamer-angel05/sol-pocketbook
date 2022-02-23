1a24b2373a72ef4aa2e220153b7855b98ead7da1
gamer-angel05 <45619299+gamer-angel05@users.noreply.github.com>
2022-02-23 04:07:20 -0600

    re:anchor mobile

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function __init__() {
 	.then( data => {
 		new formatData(data);
 		if (url) {
-			location.hash = url;
+			location.hash = "";
+			location.hash = url; /* work on mobile?? */
 		}
 		anchors.options.visible = "touch";
 		anchors.add("#scroll-content h2, #scroll-content h4");
